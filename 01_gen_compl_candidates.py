5efe5e307470d07798abed6ec5fb4b4e141ee5c9
shinji iida <physicshinzui@gmail.com>
2019-11-17 10:32:24 +0900

    adding logging

--- 01_gen_compl_candidates.py
+++ 01_gen_compl_candidates.py
@@ -7,14 +7,19 @@ import MDAnalysis
 from MDAnalysis import Universe
 from MDAnalysis.analysis import align
 from tqdm import tqdm
+import logging
+
+logging.basicConfig(filename='logging.log', level=logging.INFO)
+logging.info(f'# snapshot numbers:')
+logging.info(f'# Integrin-gtail, laminin')
 
 """
 This program modells laminin-integrin complexes, based on MD trajectories
 """
 int_gtail_ref_file  = './templates_traj/integrin_gtail.pdb'
 int_gtail_xtc_file  = '../../data_to_be_analysed/traj/xtc_files/n_all_skipped_100.xtc'
-lm_ref_file = './templates_traj/laminin.pdb'
-lm_xtc_file = '../../../laminin/data_to_be_analysed/traj/all_skipped_10.xtc'
+lm_ref_file         = './templates_traj/laminin.pdb'
+lm_xtc_file         = '../../../laminin/data_to_be_analysed/traj/all_skipped_10.xtc'
 
 u_int_gtail = Universe(int_gtail_ref_file, int_gtail_xtc_file)
 u_lm        = Universe(lm_ref_file, lm_xtc_file)
@@ -23,24 +28,27 @@ u_lm        = Universe(lm_ref_file, lm_xtc_file)
 gtail_on_integ = u_int_gtail.select_atoms('segid I and resid 1605-1609 and name CA')
 gtail_in_lm    = u_lm.select_atoms('segid C and resid 1605-1609 and name CA')
 
-#gtail_on_integ = u_int_gtail.select_atoms('segid E and resid 1605 and name CA')
-#gtain_in_lm    = u_lm.select_atoms('segid C and resid 1605 and name CA')
+snap_no_int = 0
+for frame_int_g in tqdm(u_int_gtail.trajectory[0:2]):
+    snap_no_lm = 0
+    for frame_lm in u_lm.trajectory[0:2]:
 
-for frame_int_g in tqdm(u_int_gtail.trajectory[0:1]):
-    for frame_lm in u_lm.trajectory[0:1]:
+        # The positions of the selected atoms above are centred.
         mobile = gtail_in_lm.positions    -    gtail_in_lm.atoms.center_of_mass()
         ref    = gtail_on_integ.positions - gtail_on_integ.atoms.center_of_mass()
-#        print(gtain_in_lm.atoms)
-#        print(gtail_on_integ.atoms)
-#        print(mobile)
-#        print(ref)
-        R, rmsd = align.rotation_matrix(mobile, ref)
-        print(rmsd)
 
-        # translate and rotate laminin according to the rotation matrix for gtail
+        R, rmsd = align.rotation_matrix(mobile, ref)
+
+        # Translate and rotate laminin according to the rotation matrix for gtail
         u_lm.atoms.translate(-gtail_in_lm.center_of_mass())
         u_lm.atoms.rotate(R)
         u_lm.atoms.translate(gtail_on_integ.center_of_mass())
-        u_merged = MDAnalysis.core.universe.Merge(u_lm.atoms, u_int_gtail.atoms)
-        u_merged.atoms.write("mobile_on_ref.pdb")
-#        u_int_gtail.atoms.write("mobile_on_a.pdb")
+
+        # Merge two systems laminin system and integrin-gtail one with C-alpha atoms
+        u_merged = MDAnalysis.core.universe.Merge(u_lm.atoms.select_atoms('name CA'),
+                                                  u_int_gtail.atoms.select_atoms('name CA'))
+        u_merged.atoms.write(f"aligned_{snap_no_int}-{snap_no_lm}.pdb")
+        logging.info(f'{snap_no_int} - {snap_no_lm}')
+        snap_no_lm += 1
+
+    snap_no_int += 1
