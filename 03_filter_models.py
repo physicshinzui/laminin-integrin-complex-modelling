ec184778dcf47df8da7f08a4fb685894f6bfe6e7
shinji iida <physicshinzui@gmail.com>
2020-01-15 16:36:01 +0900

    remove not-used lines.

--- 03_filter_models.py
+++ 03_filter_models.py
@@ -25,20 +25,14 @@ def pymol_process(model_numbers):
         cmd.load(f'complexes/{number}.pdb')
 
 def main():
-#    model_numbers, nViolations_of_models = reada_vio('model_no.out')
     model_no_w_scores = reada_vio('model_no.out')
     lower_bound, upper_bound = 50, 60
 
-#    filtered_model_numbers = [ model_number for model_number, n_violations in zip(model_numbers, nViolations_of_models)
-#                               if n_violations <= upper_bound and n_violations >= lower_bound]
-#    print(filtered_model_numbers, len(filtered_model_numbers))
-#    print(model_no_w_scores)
     top_models = sorted(model_no_w_scores, key=lambda x:x[1], reverse=True)[0:10]
     top_model_numbers = [i[0] for i in top_models]
     print(top_models)
     print(top_model_numbers)
 
-#    sys.exit()
     u = Universe("/Volumes/HD-siida/gtail_b1_sys/analysis/merged_aligned_complexes.pdb")
 #    u = Universe("complex_models.pdb")
     models = u.select_atoms('all')
